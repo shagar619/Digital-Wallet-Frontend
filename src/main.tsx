1e537d4c90a7617383a750782d9954a82d240fc3
shagar619 <ashagar619@gmail.com>
2026-02-03 23:35:47 +0600

    modify user table ui

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,14 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Provider as ReduxProvider } from "react-redux";
 import { ToastContainer } from "react-toastify";
-import { ThemeProvider } from "./Pages/providers/theme.provider";
 import { store } from "./redux/store";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./providers/theme.provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReduxProvider store={store}>
-        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
           <ToastContainer
             position="top-center" // bottom to center
             autoClose={3000} // auto close in 3 seconds
